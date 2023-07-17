a987fc718161ba505ea8690fd138e52a2883f0ca
Rahaditya Abimanyu Putra <rahadi.a.p123@gmail.com>
2023-07-17 14:23:12 +0700

    untuk memberi animasi pada menu saat di klik

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Halaman ini untuk javascript 
+
+// Interaksi Menu
+let menu = document.querySelector('.container .header .menuAndImage .menu');
+
+let navigasi = document.querySelector('.container .header .navigasi');
+
+menu.addEventListener('click', function() {
+  navigasi.style.transform = 'translateY(30px)';
+});
