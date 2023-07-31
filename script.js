8a503121d866d731be083b124c5a680f22e5a134
Rahaditya Abimanyu Putra <rahadi.a.p123@gmail.com>
2023-07-31 13:54:38 +0700

    menambahkan fitur dark mode

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ let navigasi = document.querySelector('.container .header .navigasi');
 menu.addEventListener('click', function() {
   navigasi.classList.toggle("aniNav");
 });
+
+
+let mode = document.querySelector('.container main .button');
+
+let span = document.querySelector('.container main .button span');
+
+
+mode.addEventListener('click', function () {
+  if(document.querySelector('html').dataset.mode === 'light'){
+    document.querySelector('html').dataset.mode = 'dark';
+    span.textContent = 'light';
+  } else{
+    document.querySelector('html').dataset.mode = 'light';
+      span.textContent = 'dark';
+  }
+});
